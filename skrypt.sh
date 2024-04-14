ffef5d8b6d9955922590aec5022d126c51db6c9e
Letycja <letycja.niemiec@gmail.com>
2024-04-14 21:27:25 +0200

    error

--- skrypt.sh
+++ skrypt.sh
@@ -31,8 +31,24 @@ fi
 
 
 #init
-if [[ "$1" == "--init" ]]; then
+if [[ "$1" == "--init" || "$1" == "-i" ]]; then
 	git clone https://github.com/Lesiiaa/narzIT_laby4 "$PWD/lnarzIT_laby4_clone"
 	PATH=$PATH:$PWD/lab_clone
 fi
 
+#--error x
+if [[ "$1" == "--error" || "$1" == "-e" ]]; then
+        if [[ -z "$2" ]]; then
+                number=100
+        else
+                number=$2
+        fi
+
+	mkdir -p errorx
+
+        for ((i=1; i<=$number; i++)); do
+                echo "Error file: $i" > "errorx/error$i.txt"
+                echo "Made by: $0" >> "errorx/error$i.txt"
+                echo "Date: $(date)" >> "errorx/error$i.txt"
+        done
+fi
