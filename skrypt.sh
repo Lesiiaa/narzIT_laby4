c47236a5c80e7f20b898125464887c76a10ae5d3
Letycja <letycja.niemiec@gmail.com>
2024-04-14 20:12:15 +0200

    logi, tworzenie plikow txt

--- skrypt.sh
+++ skrypt.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
-today=$(date +"%d-%m-%Y")
-echo $today 
+
+#date
+if [[ "$1" == "--date" ]]; then
+	echo "Today's date: $(date)"
+fi
+
+#logs
+if [[ "$1" == "--logs" ]]; then
+	if [[ -z "$2" ]]; then
+		number=100
+	else
+		number=$2
+	fi
+
+	for ((i=1; i<=$number; i++)); do
+		echo "Log file: $i" > "log$i.txt"
+		echo "Made by: $0" >> "log$i.txt"
+		echo "Date: $(date)" >> "log$i.txt"
+	done
+fi
